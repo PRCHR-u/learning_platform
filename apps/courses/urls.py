fd184ef63fdad9842d5a6a2c52af9962a79c3d9b
Killerulya2015 <Killerulya2015@yandex.ru>
2025-10-13 11:11:58 +0000

    изменение способа регистрации маршрутов

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,18 +1,10 @@
-from django.urls import path, include
 from rest_framework_nested import routers
 from .views import CourseViewSet, MaterialViewSet
 
 router = routers.SimpleRouter()
-router.register(r'courses', CourseViewSet, basename='course')
+router.register(r'courses', CourseViewSet, basename='courses')
 
-courses_router = routers.NestedSimpleRouter(
-    router, r'courses', lookup='course'
-)
-courses_router.register(
-    r'materials', MaterialViewSet, basename='course-materials'
-)
+courses_router = routers.NestedSimpleRouter(router, r'courses', lookup='course')
+courses_router.register(r'materials', MaterialViewSet, basename='course-materials')
 
-urlpatterns = [
-    path('', include(router.urls)),
-    path('', include(courses_router.urls)),
-]
+urlpatterns = router.urls + courses_router.urls
